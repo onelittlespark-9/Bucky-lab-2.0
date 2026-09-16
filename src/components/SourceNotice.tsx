8f2a7f6dbf64d1a2e5aec2768f4015784c5a2f56
onelittlespark-9 <124403135+onelittlespark-9@users.noreply.github.com>
2026-09-16 10:56:00 +0100

    Add educational data source and licensing notice

---
+++ src/components/SourceNotice.tsx
@@ -0,0 +1,2 @@
+import React from'react';
+export function SourceNotice(){return <footer className="source-notice"><a href="/sources.html">Data sources, licences, references & educational disclaimer</a></footer>}
